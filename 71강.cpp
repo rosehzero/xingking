e37a858cf7e34ae5be0ae591c27798555795f600
roeshzero <jwt501@naver.com>
2022-01-13 21:32:04 +0900

    91강 템플릿 함수 1번째 - 임의의 연산이 가능하게 해줌

--- 71강.cpp
+++ 71강.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-class Square {
+class Vector2 {
 public:
-    Square(double a) : a(a) {}
+	Vector2() : x(0), y(0) {}
+	Vector2(float x, float y) : x(x), y(y) {}
+	float GetX() const { return x; }
+	float GetY() const { return y; }
+
+	Vector2& operator+=(const Vector2& rhs) {
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+
 private:
-    double a;
+	float x, y;
 };
 
-class Rectangle : public Square {
-public:
-    Rectangle(double a, double b) : Square(a), b(b) {}
-
-private:
-    double b;
-};
+template<typename T>
+T getArraySum(const T arr[], int n) {
+	T sum = arr[0];
+	for (int i = 1; i < n; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
 
 int main() {
-    Rectangle* r = new Square(10;)
+	int iarr[5] = { 3, 1, 4, 1, 5 };
+	float farr[5] = { 3.1, 1.2, 4.3, 1.4, 5.5 };
+	Vector2 varr[3] = { Vector2(1,2), Vector2(3,4), Vector2(5,6) };
+	string sarr[3] = { "hello", "world", "doodle" };
+
+	int isum = getArraySum(iarr, 5);
+	float fsum = getArraySum<float>(farr, 5);
+	Vector2 vsum = getArraySum<Vector2>(varr, 3);
+	string ssum = getArraySum(sarr, 3);
+
+	cout << isum << endl;
+	cout << fsum << endl;
+
+	cout << vsum.GetX() << "," << vsum.GetY() << endl;
+	cout << ssum << endl;
 }
