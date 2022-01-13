d88616f700dbce6ad7395d2ed687c45567de2821
roeshzero <jwt501@naver.com>
2022-01-13 22:08:16 +0900

    91강 클래스템플릿

--- 71강.cpp
+++ 71강.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
+template<typename T>
 class Vector2 {
 public:
 	Vector2() : x(0), y(0) {}
-	Vector2(float x, float y) : x(x), y(y) {}
-	float GetX() const { return x; }
-	float GetY() const { return y; }
+	Vector2(T x, T y) : x(x), y(y) {}
+	T GetX() const { return x; }
+	T GetY() const { return y; }
+	Vector2 operator+(const Vector2<T>& rhs) const {
+		return Vector2(x + rhs.x, y + rhs.y);
+	}
+	Vector2 operator-(const Vector2& rhs) const {
+		return Vector2(x - rhs.x, y - rhs.y);
+	}
 
 	Vector2& operator+=(const Vector2& rhs) {
 		x += rhs.x;
 		y += rhs.y;
 		return *this;
 	}
+	Vector2 operator-=(const Vector2& rhs) {
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
 
 private:
-	float x, y;
+	T x, y;
 };
 
-template<typename T>
-T getArraySum(const T arr[], int n) {
-	T sum = arr[0];
-	for (int i = 1; i < n; i++) {
-		sum += arr[i];
-	}
-	return sum;
-}
-
 int main() {
-	int iarr[5] = { 3, 1, 4, 1, 5 };
-	float farr[5] = { 3.1, 1.2, 4.3, 1.4, 5.5 };
-	Vector2 varr[3] = { Vector2(1,2), Vector2(3,4), Vector2(5,6) };
-	string sarr[3] = { "hello", "world", "doodle" };
+	Vector2<float> v1(2, 3);
+	Vector2<float> v2(4, 5);
+	Vector2<float> v3 = v1 + v2;
 
-	int isum = getArraySum(iarr, 5);
-	float fsum = getArraySum<float>(farr, 5);
-	Vector2 vsum = getArraySum<Vector2>(varr, 3);
-	string ssum = getArraySum(sarr, 3);
-
-	cout << isum << endl;
-	cout << fsum << endl;
-
-	cout << vsum.GetX() << "," << vsum.GetY() << endl;
-	cout << ssum << endl;
 }
