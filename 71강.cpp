86b02606d620339964fed2bba5e595125f431d91
roeshzero <jwt501@naver.com>
2022-01-14 22:26:55 +0900

    94강 함수포인터 1

--- 71강.cpp
+++ 71강.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int fact(int n) {
-	if (n < 0) throw to_string(n) + ": 음수입니다.";
-	if (n == 0) return 1;
-	return n * fact(n - 1);
-}
-
-int comb(int n, int r) {
-	int a = fact(n);
-	int b = fact(r);
-	int c = fact(n - r);
-	return a / b / c;
+bool compare(int a, int b) {
+	return a == b;
 }
 
 int main() {
-	int n, r;
+	bool(*fp)(int, int) = compare;
 
-	try {
-		throw 12.3;
-	}
-	catch (const string& e) {
-		cout << e << endl;
-	}
-	catch (int e) {
-		cout << e << endl;
-	}
-	catch (...) {
-		cout << "알 수 없는 예외 발생" << endl;
-	}
+	bool res = fp(2, 3); // (*fp) 생략 
+
+
+	cout << res << endl;
 }
