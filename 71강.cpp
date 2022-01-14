aec959b49e2109bb6fffc549eea454f30d132b4f
roeshzero <jwt501@naver.com>
2022-01-14 21:29:38 +0900

    93강 예외처리 try catch (...)

--- 71강.cpp
+++ 71강.cpp
@@ -1,43 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-template<typename T, int n>
-class Vector {
-public:
-    T GetComp(int i) const { // i번째 성분을 리턴
-        return comp[i];
-    }
+int fact(int n) {
+	if (n < 0) throw to_string(n) + ": 음수입니다.";
+	if (n == 0) return 1;
+	return n * fact(n - 1);
+}
 
-    void SetComp(int i, T val) {
-        comp[i] = val;
-    }
-
-    Vector operator+(const Vector<T, n>& rhs) const {
-        Vector res;
-        for (int i = 0; i < n; i++) {
-            res.comp[i] = this->comp[i] + rhs.comp[i];
-            //res.SetComp(i, this->GetComp(i) + rhs.GetComp(i));
-        }
-        return res;
-    }
-
-private:
-    T comp[n]; // 벡터의 성분 
-};
+int comb(int n, int r) {
+	int a = fact(n);
+	int b = fact(r);
+	int c = fact(n - r);
+	return a / b / c;
+}
 
 int main() {
-    Vector<float, 3> v1, v2;
-    Vector<float, 2> v4;
+	int n, r;
 
-    v1.SetComp(0, 2);
-    v1.SetComp(1, 3);
-    v1.SetComp(2, 4);
-
-    v2.SetComp(0, 5);
-    v2.SetComp(1, 6);
-    v2.SetComp(2, 7);
-
-    Vector<float, 3> v3 = v1 + v2;
-
-    cout << v3.GetComp(0) << "," << v3.GetComp(1) << "," << v3.GetComp(2) << endl;
+	try {
+		throw 12.3;
+	}
+	catch (const string& e) {
+		cout << e << endl;
+	}
+	catch (int e) {
+		cout << e << endl;
+	}
+	catch (...) {
+		cout << "알 수 없는 예외 발생" << endl;
+	}
 }
