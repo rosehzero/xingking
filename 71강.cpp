af3565e43301888cba23f1cc2a8d4f02fb2943ae
roeshzero <jwt501@naver.com>
2022-01-15 09:04:02 +0900

    95강 함수객체

--- 71강.cpp
+++ 71강.cpp
@@ -1,27 +1,24 @@
-//함수포인터
+//함수객체
 
 #include <iostream>
 
 using namespace std;
 
-
-int square(int x) { return x * x; }
-int myFunc(int x) { return x * (x - 15) / 2; }
-
-int arrFnMin(const int arr[], int n, int (*f)(int)) {
-	int min = f(arr[0]);
-	for (int i = 1; i < n; i++) {
-		if (f(arr[i]) < min) {
-			min = f(arr[i]);
-		}
+class Equals {
+public:
+	Equals(int value) : value(value) {}
+	bool operator()(int x) const {
+		return x == value;
 	}
-	return min;
-}
 
+private:
+	int value;
+};
 
 
 int main() {
-	int arr[7] = { 3, 1, -4, 1, 5, 9, -2 };
-	cout << arrFnMin(arr, 7, square) << endl;
-	cout << arrFnMin(arr, 7, myFunc) << endl;
+	Equals eq(123), eq2(234);
+
+	cout << eq(123) << endl;
+	cout << eq2(234) << endl;
 }
