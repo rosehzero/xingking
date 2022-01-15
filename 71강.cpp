9a889a62a35cb0d1ed65526a41ffad17e1adb15e
roeshzero <jwt501@naver.com>
2022-01-15 09:30:14 +0900

    95강 함수포인터 > 함수객체 변환

--- 71강.cpp
+++ 71강.cpp
@@ -1,24 +1,37 @@
-//함수객체
+//함수포인터 > 함수객체 변환
 
 #include <iostream>
+#include <functional>
 
 using namespace std;
 
-class Equals {
+class Square {
 public:
-	Equals(int value) : value(value) {}
-	bool operator()(int x) const {
-		return x == value;
-	}
+	int operator()(int n) { return n * n; }
+} square;
 
-private:
-	int value;
-};
+class MyFunc {
+public:
+	int operator()(int n) { return n * (n - 15) / 2; }
+} myFunc;
+
+
+
+template<typename T>
+int arrFnMin(const int arr[], int n, T f) {
+	int min = f(arr[0]);
+	for (int i = 1; i < n; i++) {
+		if (f(arr[i]) < min) {
+			min = f(arr[i]);
+		}
+	}
+	return min;
+}
+
 
 
 int main() {
-	Equals eq(123), eq2(234);
-
-	cout << eq(123) << endl;
-	cout << eq2(234) << endl;
+	int arr[7] = { 3, 1, -4, 1, 5, 9, -2 };
+	cout << arrFnMin<Square>(arr, 7, square) << endl;
+	cout << arrFnMin<MyFunc>(arr, 7, myFunc) << endl;
 }
